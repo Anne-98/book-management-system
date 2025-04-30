2568382e1347cf01370e93001266007487869e98
Anne Sudari <58288067+Anne-98@users.noreply.github.com>
2025-04-30 15:02:52 +0530

    Set up add and getAll book routes with authMiddleware

--- server/routes/book.routes.js
+++ server/routes/book.routes.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/auth.middleware");
 const bookRoute = express.Router();
 
-console.log("book.routes.js loaded");
+// Add a book
+bookRoute.post('/add', authMiddleware, (req, res) => {
+    console.log("Book added");
+    res.status(201).json({
+        message: "Book added successfully"
+    });
+})
+
+// Get all books
+bookRoute.get('getAll', authMiddleware, (req, res) => {
+    console.log("Fetching all books");
+    res.status(200).json({
+        message: "Fetched all books successfully"
+    });
+})
 
 module.exports = bookRoute;
