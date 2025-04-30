51d170e2416eb7c02e8c64510411e446b4271098
Anne Sudari <58288067+Anne-98@users.noreply.github.com>
2025-04-30 15:15:32 +0530

    Create book model with schema

---
+++ server/models/book.models.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    publicationDate: {
+        type: Date,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }, 
+    price: {
+        type: Number,
+        required: true,
+    }
+},
+{
+    timestamps: true,
+});
+
+module.exports = mongoose.model("Book", bookSchema);
