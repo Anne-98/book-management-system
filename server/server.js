ed89066eaa7b29a5c3af1210a424f09f2734bbef
Anne Sudari <58288067+Anne-98@users.noreply.github.com>
2025-04-30 13:52:23 +0530

    Initialize server with mongodb connection

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: "your_secret_key",
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: false,
+    resave: true,
+  })
+);
+app.use(express.json());
+
+
+// Connect to MongoDB and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`⚡️ Server running on port: ${port} 🚀`);
+    });
+  })
+  .catch((err) => console.log("❌ DB connection error:", err));
